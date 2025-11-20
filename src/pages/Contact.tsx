69c17b6a100cf3eebffdd1947102732062e38ab8
faizan <muhammadfaizanali18@gmail.com>
2025-11-20 21:06:37 +0500

    Revert ".env"
    This reverts commit 707424e3d897bd2b03ff3d63494e8f145c46474a.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -51,10 +51,10 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        "service_6ntqen4",
+        "template_cvxf8le",
         templateParams,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        "6r6N8V6VRCJrRGRex"
       )
       .then(
         () => {
